dd878ea89e29d339871f65845201f9430ba7c047
Piyush Kuhad <piyushkuhad@gmail.com>
2020-07-19 03:13:39 +0530

    Added Routes

---
+++ server/src/api/logs.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+const LogEntry = require('../models/LogEntry');
+
+const router = Router();
+
+router.get('/', async (req, res, next) => {
+    // res.json({
+    //     message: '🌍'
+    // })
+    try {
+        const entries = await LogEntry.find();
+        res.json(entries);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry)
+    }
+    catch(error) {
+        //console.log(error.name); //Checks type of error
+        if(error.name === "ValidationError") {
+            res.status(422);
+        }
+        next(error);
+    }
+    //console.log(req.body);
+})
+
+module.exports = router;
--- server/src/index.js
+++ server/src/index.js
@@ -4,20 +4,24 @@ const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config()
-
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-});
+require('dotenv').config();
 
 const middlewares = require('./middlewares');
+const logs = require('./api/logs');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
 }));
+app.use(express.json()); //body-parser
 
 app.get('/', (req, res) => {
     res.json({
@@ -25,6 +29,7 @@ app.get('/', (req, res) => {
     })
 })
 
+app.use('/api/logs', logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
