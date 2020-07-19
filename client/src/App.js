2cb404844cf0cecfac53fb69949952c3b87ce115
Piyush Kuhad <piyushkuhad@gmail.com>
2020-07-19 16:58:32 +0530

    Integrated Mapbox and showing visited locations through api

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
-function App() {
+import {listLogEntries} from './Api';
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 3
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+      console.log(logEntries);
+    }
+    )();
+  }, [])
+
+  console.log('ShowPopup', showPopup);
+
+  const showAddMarkerPopup = (event) => {
+    console.log('Event', event);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+      onDblClick = {showAddMarkerPopup}
+    >
+      {
+        logEntries.map((entry) => (
+            <>
+              <Marker
+                key={entry._id} 
+                latitude={entry.latitude} 
+                longitude={entry.longitude} 
+                // offsetLeft={-12} offsetTop={-24}
+              >
+                <div
+                  onClick = {() => setShowPopup({
+                    //...showPopup,
+                    [entry._id]: true,
+                  })}
+                >
+                  <svg 
+                    className="cm-marker" 
+                    style = {{
+                      width: `${6 * viewport.zoom}`,
+                      height: `${6 * viewport.zoom}`
+                    }}
+                    viewBox="0 0 24 24" 
+                    stroke-width="2" fill="none" stroke-linecap="round" 
+                    stroke-linejoin="round"
+                  >
+                    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
+                  </svg>
+                </div>
+              </Marker>
+              {
+                showPopup[entry._id] ? ( 
+                <Popup
+                    latitude={entry.latitude} 
+                    longitude={entry.longitude} 
+                    closeButton={true}
+                    closeOnClick={false}
+                    // onClose = {() => setShowPopup({
+                    //   ...showPopup,
+                    //   [entry._id]: false,
+                    // })}
+                    onClose={() =>  setShowPopup({})}
+                    dynamicPosition = {true}
+                    anchor="top" >
+                    <div className="cm-popup">
+                      <h3>{entry.title}</h3>
+                      <p>{entry.comments}</p>
+                      <small>Visited on: {new Date(entry.visitDate).toLocaleDateString()}</small>
+                    </div>
+                </Popup>
+                )
+                : null
+              }
+            </>
+          )
+        )
+      }
+    </ReactMapGL>
   );
 }
 
